ad397ab0dc51f97fc381e4eb5cfdee017862957f
HongpengYu <480814177@qq.com>
2018-01-12 18:00:48 +0800

    Initial Commit

---
+++ PPSocketManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PPSocketManager
+//
+//  Created by HongpengYu on 2018/1/12.
+//  Copyright © 2018年 HongpengYu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PPSocketManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PPSocketManager
+//
+//  Created by HongpengYu on 2018/1/12.
+//  Copyright © 2018年 HongpengYu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
